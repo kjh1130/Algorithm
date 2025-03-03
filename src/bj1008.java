63cf35d3b294bffe73046bec565ddc3019838116
JihyunKim <kimwlgus001130@gmail.com>
2025-03-03 16:36:44 +0900

    baekjoon #1008

--- src/bj1008.java
+++ src/bj1008.java
@@ -1,3 +1,12 @@
-public class bj1008 {
+import java.util.Scanner;
 
+public class bj1008 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double A = scanner.nextInt();
+        double B = scanner.nextInt();
+        scanner.close();
+
+        System.out.println(A / B);
+    }
 }
