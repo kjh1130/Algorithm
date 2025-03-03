9c12f74086950988da2abc067ed292b133020b0d
JihyunKim <kimwlgus001130@gmail.com>
2025-03-03 22:29:59 +0900

    baekjoon #1009

--- src/bj1009.java
+++ src/bj1009.java
@@ -2,32 +2,26 @@ import java.util.Scanner;
 
 public class bj1009 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+        Scanner scanner = new Scanner(System.in);
 
-        int[] a = new int[T];
-        int[] b = new int[T];
-        for (int i = 0; i < T; i++) {
-            a[i] = sc.nextInt();
-            b[i] = sc.nextInt();
+        int testcase = scanner.nextInt();
+
+        for (int i = 0; i < testcase; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int c = a;
+
+            for (int j = 0; j < b - 1; j++) {
+                a = (a % 10) * c;
+            }
+
+            if (a % 10 == 0) {
+                System.out.println(10);
+            } else {
+                System.out.println(a % 10);
+            }
         }
-        for (int j = 0; j < T; j++) {
-            System.out.println(lastComputerNum(a[j], b[j]));
-        }
-        sc.close();
-    }
-    public static int lastComputerNum (int a, int b) {
-        int lastDigit = a % 10;
 
-        int[][] patterns = {{0}, {1}, {2,4,6,8}, {3,9,7,1}, {4,6}, {5}, {6}, {7,9,3,1}, {8,4,2,6},{9,1}};
-        int[] cycle = patterns[lastDigit];
-        int cycleLength = cycle.length;
-
-        int index = (b-1) % cycleLength;
-        int result = cycle[index];
-
-        return result == 0? 10: result;
+        scanner.close();
     }
 }
-
-
